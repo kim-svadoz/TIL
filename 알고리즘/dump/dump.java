c29cf9c7025d4c2c4173ff912ebadd532ec15ffd
kim_svadoz <dhkdghehfdl@gmail.com>
2020-10-24 20:12:23 +0900

    tiny change

--- 알고리즘/dump/dump.java
+++ 알고리즘/dump/dump.java
@@ -1,23 +1,46 @@
 public class Main{
-    public static void main(String[] args){
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-		int[] plays = {500, 600, 150, 800, 2500};
-		solution(genres, plays);
-    }
-    public static int[] solution(String[] genres, int[] plays) {
-        Map<String, Object> genersMap = new HashMap<>();    // 장르 , 곡정보
-        Map<String, Integer> playMap = new HashMap<>();     // 장르, 총 장르 재생 수 
-        ArrayList<Integer> resultAL = new ArrayList<>();
-        for(int i=0; i<genres.length; i++){
-            String key = genres[i];
-            Hashmap<Integer, Integer> infoMap;      // 곡번호, 재생횟수
-                
-            if(genresMap.containsKey(key)) {
-                infoMap = (HashMap<Integer, Integer>) genersMap.get(key);
-            } else {
-                infoMap = new HashMap<Integer, Integer>();
+    private static int[] parent;
+    private static int[] cnt;
+
+    public void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamRedaer(System.in));
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st;
+        HashMap<String, Integer> map;
+        int testcase = Integer.parseInt(br.readLine());
+
+        while(testcase-- > 0){
+            int F = Integer.parseInt(br.readLine());
+            map = new HashMap<>();
+            cnt = new int[F*2 + 1];
+            parent = new int[F*2 + 1];
+            for(int i=1,index=1; i<=F; i++){
+                st = new StringTokenizer(br.readLine());
+                String[] strs = {st.nextToken(), st.nextToekn()};
+                for(int j=0; j<2; j++){
+                    if(!map.containsKey(strs[j])) {
+                        cnt[index] = 1;
+                        parent[index] = index;
+                        map.put(strs[j], index++);
+                    }
+                }
+                int u = map.get(strs[0]);
+                int v = map.get(strs[1]);
+                union(u, v);
+                sb.append(cnt[find(u)]).append("\n");
             }
-            
         }
+        System.out.println(sb.toString());
+    }
+    public static void union(int u, int v){
+        u = find(u);
+        v = find(v);
+        if(u == v) return;
+        parent[u] = v;
+        cnt[v] += cnt[u];
+    }
+    public static void find(u){
+        if(parent[u]==u) return u;
+        return parent[u]=find(parent[u]);
     }
 }
