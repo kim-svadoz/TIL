4d00bc882d40e5641cd2a6f64af3586c14d8f591
ksh <dhkdghehfdl@gmail.com>
2021-05-20 22:41:38 +0900

    add trivial image

---
+++ Algorithm/EnterpriseTest/KRAFTON_2021H_01.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class KRAFTON_2021H_01 {
+    public static void main(String[] args) throws IOException {
+        one two three four five
+
+        five four one three two
+        5 4 1 3 2
+    }
+}
---
+++ Algorithm/EnterpriseTest/KRAFTON_2021H_03.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class KRAFTON_2021H_03 {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
